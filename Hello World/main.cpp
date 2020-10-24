844e673b81f820493d0759e3c90383c4b3d214e7
MaxHel <maxime.heliotpro@gmail.com>
2020-10-24 19:15:54 +0200

    Hello World

---
+++ Hello World/main.cpp
@@ -0,0 +1,14 @@
+// Hello world
+// A basic program
+
+// take a file call iostream and put it here before compiling our program
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // endl create a blank line after printing Hello world!
+    cout << "Hello world!" << endl;
+
+    return 0;
+}

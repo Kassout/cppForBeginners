855272b2f0ea853083e5a6d4542bba21f3604794
MaxHel <maxime.heliotpro@gmail.com>
2020-10-27 14:25:41 +0100

    for

---
+++ for/main.cpp
@@ -0,0 +1,13 @@
+// Name     : for.cpp
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int i = 0; i < 10; i++) {
+        cout << "Hello " << i << endl;
+    }
+
+    return 0;
+}

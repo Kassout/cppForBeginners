754a2b8b3b75b16844d1e47f6195e511952bf140
MaxHel <maxime.heliotpro@gmail.com>
2020-10-27 14:20:49 +0100

    while

---
+++ while/main.cpp
@@ -0,0 +1,18 @@
+// None     : while.cpp
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int i = 0;
+
+    while(i < 5) {
+        cout << "Hello" << endl;
+        i++;
+    }
+
+    cout << "Programming quitting." << endl;
+
+    return 0;
+}

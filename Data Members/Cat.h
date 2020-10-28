f860411e0a06e3aafc2303bc178029a41e357d49
MaxHel <maxime.heliotpro@gmail.com>
2020-10-28 16:52:19 +0100

    Data Members

---
+++ Data Members/Cat.h
@@ -0,0 +1,15 @@
+#ifndef DATA_MEMBERS_CAT_H
+#define DATA_MEMBERS_CAT_H
+
+
+class Cat {
+private:
+    bool happy;
+public:
+    void makeHappy();
+    void makeSad();
+    void speak();
+};
+
+
+#endif //DATA_MEMBERS_CAT_H

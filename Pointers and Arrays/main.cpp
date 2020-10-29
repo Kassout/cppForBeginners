42f43353099b527f1555fc42815cb8971bf41819
MaxHel <maxime.heliotpro@gmail.com>
2020-10-29 19:59:14 +0100

    Update Pointers and Arrays

--- Pointers and Arrays/main.cpp
+++ Pointers and Arrays/main.cpp
@@ -7,6 +7,37 @@ using namespace std;
 // Loop through an array by incrementing a pointer
 // Loop through an array, stopping by comparing two pointers
 
+void loopWithReference(string list[], int listSize) {
+
+    string *pList = list;
+    for (int i = 0; i < listSize / sizeof(string); i++) {
+        cout << pList[i] << " " << flush;
+    }
+}
+
+void loopWithIncrementingPointer(string list[], int listSize) {
+
+    string *pList = list;
+    for (int i = 0; i < listSize / sizeof(string); i++, pList++) {
+        cout << *pList << " " << flush;
+    }
+}
+
+void loopWithComparingPointers(string list[], int listSize) {
+    string *pElement = &list[0];
+    string *pEnd = &list[(listSize / sizeof(string)) - 1];
+
+    while(true) {
+        cout << *pElement << " " << flush;
+
+        if(pElement == pEnd) {
+            break;
+        }
+
+        pElement++;
+    }
+}
+
 int main() {
 
     string texts[] = {"one", "two", "three"};
@@ -39,6 +70,15 @@ int main() {
 
         pElement++;
     }
+
+    cout << endl << "===========================" << endl;
+
+    string list[] = {"cat", "dog", "fish", "squirrel", "hyena", "lizard", "lion", "bear"};
+    loopWithReference(list, sizeof(list));
+    cout << endl;
+    loopWithIncrementingPointer(list, sizeof(list));
+    cout << endl;
+    loopWithComparingPointers(list, sizeof(list));
 
     return 0;
 }

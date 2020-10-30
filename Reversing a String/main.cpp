0c8031d2afd96ee5d9f18c5d6e8da2f1a19d10e1
MaxHel <maxime.heliotpro@gmail.com>
2020-10-30 14:02:46 +0100

    Reversing a String

---
+++ Reversing a String/main.cpp
@@ -0,0 +1,30 @@
+// Name     : Reversing.cpp
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char text[] = "Hello there!";
+
+    int nChars = sizeof(text) - 1;
+
+    char *pStart = text;
+    char *pEnd = text + nChars - 1;
+
+    cout << *pEnd << endl;
+
+    while(pStart < pEnd) {
+
+        char save = *pStart;
+        *pStart = *pEnd;
+        *pEnd = save;
+
+        pStart++;
+        pEnd--;
+    }
+
+    cout << text << endl;
+
+    return 0;
+}

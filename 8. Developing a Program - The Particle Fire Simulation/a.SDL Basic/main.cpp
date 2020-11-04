908b9431023b703a71ba8820826dcfae9fb65ba3
MaxHel <maxime.heliotpro@gmail.com>
2020-11-04 15:48:35 +0100

    part a + b section 8

--- 8. Developing a Program - The Particle Fire Simulation/a.SDL Basic/main.cpp
+++ 8. Developing a Program - The Particle Fire Simulation/a.SDL Basic/main.cpp
@@ -6,7 +6,43 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!" << endl;
+
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		cout << "SDL Init failed." << endl;
+		return 1;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Particle Fire Explosion", 
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+		SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	if (window == NULL) {
+		SDL_Quit();	
+		return 2;
+	}
+
+	bool quit = false;
+
+	SDL_Event event;
+
+	while (!quit) {
+		// Update particles
+		// Draw particles
+		// Check for messages/events
+		
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quit == true;
+			}
+		}
+	
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
 	return 0;
 }
 
